48cb5051e603a3b5f629e0f1c94b3ce2d9e70102
kalganem <khaled.a.alganem@gmail.com>
2021-02-28 13:03:44 -0500

    added map_file and cov_file to krsa func

--- R/krsa.R
+++ R/krsa.R
@@ -5,6 +5,8 @@
 #' @param data krsa data_modeled$scaled
 #' @param samples sample names
 #' @param lfc_thr LFC cutoff for plot
+#' @param map_file kinase-substrate mapping file
+#' @param cov_file LFC cutoff for plot
 #'
 #' @return ggplot
 #'
@@ -16,13 +18,13 @@
 #' TRUE
 
 
-krsa <- function(y, itr = 2000, seed = 123, return_count = F) {
+krsa <- function(y, itr = 2000, seed = 123, return_count = F, map_file = KRSA_file, cov_file = chipCov) {
   message("Running KRSA ...")
   set.seed(seed)
-  purrr::map_df(1:itr,krsa_sampling,chipCov ,KRSA_file,length(y)) -> temp
+  purrr::map_df(1:itr,krsa_sampling,cov_file ,map_file,length(y)) -> temp
   temp %>% group_by(Kin) %>% summarise(SamplingAvg = mean(counts), SD= sd(counts)) -> temp2
 
-  temp3 <- chipCov %>%
+  temp3 <- cov_file %>%
     group_by(Kin) %>%
     summarise(
       Observed = sum(Substrates %in% y)
