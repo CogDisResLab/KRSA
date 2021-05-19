cb4589568a1006ca6437bb5edc00ec0859f4a411
kalganem <khaled.a.alganem@gmail.com>
2021-05-19 18:57:39 -0400

    added ce output for peptides

--- R/krsa_ce_output_peptides.R
+++ R/krsa_ce_output_peptides.R
@@ -3,19 +3,20 @@
 #' Takes in the Z score table and output kinase with their scores
 #'
 #' @param peptide_table a df of the log2 FC scores of peptides
+#' @param metric which col to use as the score (totalMeanLFC, LFC, ... etc)
 #' @param file_name file name and path to save file as txt tab delimited file
 #'
 #' @return saved txt file to be used in creedenzymatic
 #'
-#'
+#' @export
 #'
 #'
 
-krsa_ce_output_peptides <- function(peptide_table, file_name) {
+krsa_ce_output_peptides <- function(peptide_table, metric, file_name) {
 
   peptide_table %>%
-    dplyr::select(Peptide) %>%
-    dplyr::rename(Score = Z) %>%
+    dplyr::select(Peptide, {{ metric }}) %>%
+    dplyr::rename(Score = {{ metric }}) %>%
     readr::write_delim(file=file_name)
 
 }
