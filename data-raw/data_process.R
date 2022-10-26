5a216c4b64647bc65b5ce4745a55dc2b7d75dd81
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2022-10-26 16:19:00 -0400

    updated the included datasets

--- data-raw/data_process.R
+++ data-raw/data_process.R
@@ -1,31 +1,26 @@
-library(readr)
+library(tidyverse)
 
+KRSA_Mapping_STK_PamChip_87102_v1 <- read_csv("KRSA_Mapping_STK_PamChip_87102_v1.csv")
 
-KRSA_Mapping_STK_PamChip_87102_v1 <- read.table("data-raw/KRSA_Mapping_STK_PamChip_87102_v1.txt",
-                        header = TRUE, sep = "\t", as.is=T, quote = "\"",
-                        check.names=FALSE, stringsAsFactors=FALSE)
+KRSA_Mapping_PTK_PamChip_86402_v1 <- read_csv("KRSA_Mapping_PTK_PamChip_86402_v1.csv")
 
-KRSA_Mapping_PTK_PamChip_86402_v1 <- read.table("data-raw/KRSA_Mapping_PTK_PamChip_86402_v1.txt",
-                            header = TRUE, sep = "\t", as.is=T, quote = "\"",
-                            check.names=FALSE, stringsAsFactors=FALSE)
+KRSA_coverage_STK_PamChip_87102_v1 <- readRDS("KRSA_coverage_STK_PamChip_87102_v1.rds")
 
-KRSA_coverage_STK_PamChip_87102_v1 <- readRDS("data-raw/KRSA_coverage_STK_PamChip_87102_v1.rds")
+KRSA_layout_STK_PamChip_87102_v1 <- readr::read_tsv("STK-87102-Array-Layout.txt")
 
-KRSA_layout_STK_PamChip_87102_v1 <- readr::read_tsv("data-raw/STK-87102-Array-Layout.txt")
+KRSA_layout_PTK_PamChip_86402_v1 <- readr::read_tsv("PTK-86402-Array-Layout.txt")
 
-KRSA_layout_PTK_PamChip_86402_v1 <- readr::read_tsv("data-raw/PTK-86402-Array-Layout.txt")
+KRSA_uka_mapping_PTK_PamChip_86402_v1 <- readr::read_csv("uka_pep2kinase_PTK.csv")
 
-KRSA_uka_mapping_PTK_PamChip_86402_v1 <- readr::read_csv("data-raw/uka_pep2kinase_PTK.csv")
-
-KRSA_uka_mapping_STK_PamChip_87102_v1 <- readr::read_csv("data-raw/uka_pep2kinase_STK.csv")
+KRSA_uka_mapping_STK_PamChip_87102_v1 <- readr::read_csv("uka_pep2kinase_STK.csv")
 
 #removed pdk
 KRSA_coverage_STK_PamChip_87102_v1 %>% dplyr::filter(Kin != "PDK") -> KRSA_coverage_STK_PamChip_87102_v2
 
-KRSA_coverage_PTK_PamChip_86402_v1 <- readRDS("data-raw/KRSA_coverage_PTK_PamChip_86402_v1.rds")
+KRSA_coverage_PTK_PamChip_86402_v1 <- readRDS("KRSA_coverage_PTK_PamChip_86402_v1.rds")
 
-ballModel_nodes <- readRDS("data-raw/FinNodes_Final_FF.rds")
-ballModel_edges <- readRDS("data-raw/FinEdges_Final_FF.rds")
+ballModel_nodes <- readRDS("FinNodes_Final_FF.rds")
+ballModel_edges <- readRDS("FinEdges_Final_FF.rds")
 
 usethis::use_data(KRSA_Mapping_STK_PamChip_87102_v1,
                   KRSA_Mapping_PTK_PamChip_86402_v1,
