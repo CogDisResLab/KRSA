1dd4907d832bd78424f0ae582cd7601777e4acad
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2024-06-23 23:23:29 -0400

    feat: update the stored datasets
    Signed-off-by: Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>

--- R/enrichr_lib_call.R
+++ R/enrichr_lib_call.R
@@ -1,6 +1,8 @@
 #' Runs enrichr api (single GET call for a geneset library)
 #'
-#' This function takes in the geneset library name from Enrichr and userListId (id created by enrichr api) sand retruns tidy dataframe of all geneset terms in that library
+#' This function takes in the geneset library name from Enrichr and
+#' userListId (id created by enrichr api) sand retruns tidy dataframe of
+#' all geneset terms in that library
 #'
 #' @param lib geneset library name from Enrichr
 #' @param userListId id created by the POST enrichr api call
@@ -29,7 +31,10 @@ enrichr_lib_call <- function(lib, userListId) {
 
     base::message(base::paste0(lib, " ..."))
 
-    res <- base::suppressMessages(jsonlite::fromJSON(httr::content(httr::GET(full_link), as = "text")))
+    res <- base::suppressMessages(jsonlite::fromJSON(httr::content(
+        httr::GET(full_link),
+        as = "text"
+    )))
 
     purrr::map(res[[1]], process_output) %>%
         purrr::map_df(dplyr::bind_rows) %>%
